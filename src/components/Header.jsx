ce8ab26937efafeedb79743fc420eee8171fc20d
audukehinde <auduomohkehinde@gmail.com>
2024-12-12 00:18:37 +0100

    Header section completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="src/assets/react-2.svg" alt="Site logo" />
+                <span>My Travel Journal</span>
+            </nav>
+        </header>
+    )
+}
